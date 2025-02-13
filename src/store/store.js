d058b8c5d7a805b9e8ae8d057e52ea42d5489b2f
Nikhil Bhagoria <nikhilkumar2450@gmail.com>
2025-02-13 21:33:50 +0530

    Initial commit: Interview Scheduler application

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import interviewReducer from './interviewSlice';
-
-export const store = configureStore({
-    reducer: {
-        interviews: interviewReducer,
-    },
-}); 
